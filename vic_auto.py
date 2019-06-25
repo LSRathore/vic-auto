e950e9c69cc78316f0c5c496d5691acba611d1f0
Lokendra singh rathore <lokendra.nalu@gmail.com>
2019-06-25 16:28:17 +0530

    Update vic_auto.py
    Added elevation band file script

--- vic_auto.py
+++ vic_auto.py
@@ -1,15 +1,16 @@
+
 """
 Created on Sat Apr 13 2019
 @author: Lokendra Singh Rathore
 @National Institute of Hydrology, Roorkee, India
 @email: lokendra.nalu@gmail.com
-
 This script is to generate vegetation parameter and soil parameter file 
 for Variable Infiltration Capacity (VIC) hydrologic model
 It does not generate Forcing files
 The script is written in Python 2.7 and has been tested on ArcGIS version 10.5
 If you have Python 3, then change put the parantheses after print command allover otherwise it will not run
 """
+
 
 #############################################################################################################################
 #Following python libraries should be installed
@@ -26,17 +27,17 @@ import numpy as np
 ### Make sure, there is not any space in the file or folder name (use _ insted)
 
 
-workspace="G:\\Rathore\Hosein_input\\inputs\\out1"          ## Put location a folder, all files will be saved in there (TIP: Create a fresh folder)
-shape_file="G:\\Rathore\\Hosein_input\\inputs\\shapfile\\2050724400.shp"        #Shapefile of the basin area. Make sure its cordinates are GCS WGS 1984
-dem="G:\\Rathore\\Hosein_input\\inputs\\Dem\\Dem_Mashhad.tif"                  #DEM "  "  "  "  "  "        "    "        "        "      "
-lulc="G:\\Rathore\\Hosein_input\\inputs\\LuLc\\mashhad_lulc_newpro.tif"          #LULC image of area  (TO MAKE VEG PARAMETER FILE)
-soil="G:\\Rathore\\Hosein_input\\inputs\\soil\\mashhad-basin.HWSD.tif"   #SOIL Image  (To make soil parameter file)
+workspace="M:\\rathore\\vic20km\\gis"          ## Put location a folder, all files will be saved in there (TIP: Create a fresh folder)
+shape_file="G:\\Rathore\\vic_auto5\\shape_proj.shp"        #Shapefile of the basin area. Make sure its cordinates are GCS WGS 1984
+dem="G:\\Rathore\\vic_auto5\\dem_extfill"                  #DEM "  "  "  "  "  "        "    "        "        "      "
+lulc="G:\\Rathore\\swat_inp\\soil_lulc\\lulc.tif"          #LULC image of area  (TO MAKE VEG PARAMETER FILE)
+soil="M:\\rathore\\vic20km\gis\\soil_fao_sb.tif"   #SOIL Image  (To make soil parameter file)
 rooting_depth_csv="C:\\Python27\\ArcGIS10.5\\vic_auto\\RootingDepths.csv"  ##Root depth csv file, provided with the docs
 soil_appendix="C:\\Python27\\ArcGIS10.5\\vic_auto\\soil_appendix_3layer.xlsx"  ##Soil appendix, provied with docs
 
 ###Cell height and width in degree, Edit acc
-cell_h=0.1 
-cell_w=0.1
+cell_h=0.25
+cell_w=0.25
 
 ##########################################################################################################################
 ## DO NOT EDIT ANYTHING##
@@ -68,14 +69,14 @@ arcpy.AddField_management("fishnet_f.shp", "run_grid", "SHORT", 0, "", "", "refc
 print "Calculating area of each grid cell after union... \n"
 arcpy.CalculateField_management("union.shp","area1",'!shape.area!',"PYTHON")
 temp_layer=arcpy.MakeFeatureLayer_management("union.shp","temp_lyr")
-arcpy.SelectLayerByAttribute_management(temp_layer,"NEW_SELECTION","basin_pt=1")
+arcpy.SelectLayerByAttribute_management(temp_layer,"NEW_SELECTION","basin_pt=1 AND area1>= 10000000")
 print "Making run grid... \n"
 arcpy.CopyFeatures_management(temp_layer,"new_run_grid")
 arcpy.AddField_management("new_run_grid.shp", "run_grid1", "SHORT", 0, "", "", "refcode", "NULLABLE", "REQUIRED")
 arcpy.CalculateField_management("new_run_grid.shp","run_grid1",'1')
 
 arcpy.DeleteField_management(shape_file,"basin_pt")
-#arcpy.DeleteField_management("new_run_grid.shp","basin_pt")
+
 d= {k:v for k,v in arcpy.da.SearchCursor("new_run_grid.shp",["FID_Fishne","run_grid1"])}
 
 with arcpy.da.UpdateCursor("fishnet_f.shp",["FID","run_grid"]) as cursor:
@@ -90,8 +91,7 @@ print "Calculating elevation ...\n"
 arcpy.AddField_management("fish_lyr","ELEVATION","FLOAT")
 arcpy.gp.ZonalStatisticsAsTable_sa("fish_lyr","FID",dem,"ele_tab",'DATA','MEAN')
 
-#print "list of column names in fishnet layer", [aa.name for aa in arcpy.ListFields("fish_lyr")], "\n\n\n"
-#print "list of col names for elevation zonal table", [aa.name for aa in arcpy.ListFields("ele_tab")], "\n"
+
 ddd={k:v for k,v in arcpy.da.SearchCursor("ele_tab",["FID","MEAN"])}
 with arcpy.da.UpdateCursor("fish_lyr",["FID","ELEVATION"]) as cursor:
     for row in cursor:
@@ -129,10 +129,11 @@ arcpy.AddJoin_management("fish_lyr","FID","slope_tab","FID")
 arcpy.CalculateField_management("fish_lyr","fishnet_f.slope","!slope_tab:MEAN!","PYTHON")
 arcpy.RemoveJoin_management("fish_lyr","slope_tab")
 
-print "Creating final fishnet (fishnet_final.shp) which consists rungrid, lat, lon, elevation, soil and slope values for all cell ... \n"
+print "Creating final fishnet (fishnet_final.shp) which consists rungrid, lat, lon, elevation, soil and slope values for all run cell ... \n"
 arcpy.CopyFeatures_management("fish_lyr","fishnet_final.shp")
 arcpy.AddField_management("fishnet_final.shp","area","FLOAT")
 arcpy.CalculateField_management("fishnet_final.shp","area","!shape.area!","PYTHON")
+
 
 print "Preparing soil parameter file, dont open any files until it completes \n"
 arcpy.ExcelToTable_conversion(soil_appendix,"soil_app1")
@@ -202,7 +203,60 @@ for i in range(grid_data.shape[0]):
 df.replace(-9999," ").to_excel("vegparam_python_test.xlsx",header=None,index=None)
 
 print "Completed making vegetation par file (vegparam_python_test.xlsx), located in workspace\n"
-print "Convert the excel file to tab delimited text file for VIC"
-print "Vegetation parameter file and soil parameter file are generated successfully. Remove the header line from soil par file"
-print "****************************************************************"
-############################################################################################################################
+print "Convert the excel file to tab delimited text file for VIC\n"
+print "Vegetation parameter file and soil parameter file have been generated successfully. Remove the header line from soil par file\n"
+print "****************************************************************************************\n"
+
+############## SCRIPT FOR SONW/ELEVATION BAND FILE GENERATION ###################
+user=input("\nDo you want to make snow/elevation band file? (type 0 for no, 1 for yes): \n")
+if user==1:
+    user_i=input("Type the number of elevation bands required: \n")
+    dem_ext=arcpy.sa.ExtractByMask(dem,"fishnet_f.shp")
+    dem_ext.save("dem_extfish.tif")
+    dem_desc=arcpy.sa.Raster("dem_extfish.tif")
+    d1=dem_desc.minimum
+    d2=dem_desc.maximum
+    band_dif=math.ceil((d2-d1)/user_i)
+    remap=[]
+    r1=d1
+    print "The elevation band file will have the following range of bands: \n"
+    for i in range(user_i):
+        r= [r1+(i*band_dif), r1+band_dif*(i+1),i+1]
+        print r
+        remap.append(r)
+    
+
+    dem_re=arcpy.sa.Reclassify("dem_extfish.tif","VALUE",arcpy.sa.RemapRange(remap))
+    dem_re.save("reclass_dem_eleband.tif")
+    
+    print "Elevation band file is being processed... \n"
+    arcpy.RasterToPolygon_conversion(in_raster=dem_re,raster_field='VALUE',out_polygon_features="ras2poly_redem",simplify="NO_SIMPLIFY")
+    arcpy.sa.TabulateArea("fishnet_f.shp","FID", "reclass_dem_eleband.tif", "VALUE","tab_area_eleband")
+    arcpy.Union_analysis(["ras2poly_redem.shp","fishnet_f.shp"],"union_elebnd")
+    arcpy.gp.ZonalStatisticsAsTable_sa('union_elebnd.shp', 'FID_ras2po', 'dem_extfish.tif', "ele_eachzonetab1", 'DATA', 'MEAN')
+    arcpy.JoinField_management("union_elebnd.shp","FID_ras2po","ele_eachzonetab1","FID_ras2po")
+    arcpy.TableToExcel_conversion("union_elebnd.shp","temp_eb.xls")
+    
+    temp_eb=pd.read_excel("temp_eb.xls")
+    temp1=temp_eb[(temp_eb["run_grid"]>=0) & (temp_eb["FID_ras2po"]>=0)]
+    temp2=temp1[["FID","FID_ras2po","FID_fishne","run_grid","MEAN",'gridcode']]
+    pivot=pd.pivot_table(temp2,index=["FID_fishne","run_grid"],columns="gridcode",values="MEAN",aggfunc=np.mean,fill_value=0)
+    
+    arcpy.TableToExcel_conversion("tab_area_eleband","tabulate_area_temp.xls")
+    tab_area=pd.read_excel("tabulate_area_temp.xls")
+    tab_area["sum"]=tab_area.iloc[:,2:].sum(axis=1)
+    df_rand=pd.DataFrame()
+    for i in range(user_i):
+        df_rand[str(i)]=tab_area.iloc[:,i+2]/tab_area["sum"]
+
+    pivot=pivot.reset_index()
+    pivot=pivot.drop("FID_fishne",axis=1)
+    df_rand["gridcode"]=pivot.index
+    pivot["gridcode"]=df_rand["gridcode"]
+    dfm1=df_rand.merge(pivot,on="gridcode")
+    dfm2=dfm1.merge(df_rand,on="gridcode")
+    dfm2=dfm2[dfm2["run_grid"]==1]
+    dfm2=dfm2.drop(["gridcode","run_grid"],axis=1)
+    dfm2.to_csv("elebandfile_"+str(user_i),header=None,sep="\t")
+    print "Elevation/snow band file with mentioned band numbers, has been prepared with the name of \n"
+    print "elebandfile_"+str(user_i)+ "in the workspace."
